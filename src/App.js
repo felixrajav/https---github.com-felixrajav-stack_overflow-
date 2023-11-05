984ecbb51949308db9b1ddd8744d6e8ac4ed214a
felixrajav <felixvictorraj@gmail.com>
2023-11-05 21:18:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
+import AllRoutes from './AllRoutes'
+import { fetchAllQuestions } from './actions/question'
+import { fetchAllUsers } from './actions/users'
+import Footer from './Pages/Footer/Footer'
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchAllQuestions())
+    dispatch(fetchAllUsers())
+  }, [dispatch])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router >
+        <Navbar />
+        <AllRoutes />
+        <Footer />
+      </Router >
     </div>
   );
 }
